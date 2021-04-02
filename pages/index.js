fdfb8956a9fa5e774e830b87b48e24a96d521bb4
Omar <omaar_5@hotmail.com>
2021-04-02 15:33:17 +0200

    sidebar part 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import HeadTag from "../HeadTag";
+import styled from "styled-components";
+import { Sidebar } from "../components";
 
 export default function Home() {
   return (
-    <div>
+    <HomeContainer>
       <HeadTag title="WhatsApp 2.0" />
-      <h1>Starting the build</h1>
-    </div>
-  )
-}
+      
+      <Sidebar />
+    </HomeContainer>
+  );
+};
+
+const HomeContainer = styled.div`
+  
+`;
