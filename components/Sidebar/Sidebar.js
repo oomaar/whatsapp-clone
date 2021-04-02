f7763c1a12f947697c936bdfb1338af188a17657
Omar <omaar_5@hotmail.com>
2021-04-02 16:35:24 +0200

    sidebar part 2

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import { IconButton } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
+import * as EmailValidator from "email-validator";
 import {
     Container,
     Header,
@@ -13,6 +14,16 @@ import {
 } from "./styledSidebar";
 
 const Sidebar = () => {
+    const createChat = () => {
+        const input = prompt('Please enter an email addres for the user you want to chat with');
+
+        if (!input) return null;
+
+        if (EmailValidator.validate(input)) {
+            // Push the chat to the database 'chats' collection
+        };
+    };
+
     return (
         <Container>
             <Header>
@@ -30,7 +41,7 @@ const Sidebar = () => {
                 <SearchIcon />
                 <SearchInput placeholder="Search in chats" />
             </Search>
-            <SidebarButton>Start a new chat</SidebarButton>
+            <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
             {/* List Of Chats Goes Here */}
         </Container>
     );
