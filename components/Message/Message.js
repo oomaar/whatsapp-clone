8680e5603f9019c4e6a9905fac70a93579479358
Omar <omaar_5@hotmail.com>
2021-04-03 22:11:21 +0200

    messageComponent part 2

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,11 +1,26 @@
+import moment from "moment";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../lib/firebase";
 import {
     Container,
+    MessageElement,
+    Sender,
+    Reciever,
+    TimeStamp,
 } from "./styledMessage";
 
 const Message = ({ user, message }) => {
+    const [userLoggedIn] = useAuthState(auth);
+    const TypeOfMessage = user === userLoggedIn.email ? Sender : Reciever;
+
     return (
         <Container>
-            <h1>Message Screen</h1>
+            <TypeOfMessage>
+                {message.message}
+                <TimeStamp>
+                    {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
+                </TimeStamp>
+            </TypeOfMessage>
         </Container>
     );
 };
--- components/Message/styledMessage.js
+++ components/Message/styledMessage.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const MessageElement = styled.p`
+    width: fit-content;
+    padding: 15px 15px 26px;
+    border-radius: 8px;
+    margin: 10px;
+    min-width: 60px;
+    position: relative;
+    text-align: right;
+`;
+
+export const Sender = styled(MessageElement)`
+    margin-left: auto;
+    background-color: #dcf8c6;
+`;
+
+export const Reciever = styled(MessageElement)`
+    background-color: whitesmoke;
+    text-align: left;
+`;
+
+export const TimeStamp = styled.span`
+    color: gray;
+    padding: 10px;
+    font-size: 10px;
+    position: absolute;
+    bottom: 0;
+    text-align: right;
+    right: 0;
+`;
